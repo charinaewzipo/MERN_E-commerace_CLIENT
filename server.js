fdf4b08db07851d1d52b8b55b8a085fb9f6624ef
charin <charin.aewzipo@gmail.com>
2022-08-14 14:26:01 +0700

    deploy heroku

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 const express = require("express");
 const compression = require("compression");
 const path = require("path");
-const cors = require("cors");
+
 const app = express();
 
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions)); // Use this after the variable declaration
 app.use(compression());
 app.use(express.static(path.join(__dirname, "build")));
 app.get("*", (req, res) => {
