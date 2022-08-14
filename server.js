f0d7be99d8953acd5909800cda64b9a6c56c0f95
charin <charin.aewzipo@gmail.com>
2022-08-15 00:10:47 +0700

    make better

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 // const express = require("express");
 // const compression = require("compression");
 // const path = require("path");
-// dotenv.config();
+// const cors = require("cors");
 // const app = express();
+// require("dotenv").config();
 
+// app.use(cors()); // Use this after the variable declaration
 // app.use(compression());
 // app.use(express.static(path.join(__dirname, "build")));
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "build", "index.html"));
 // });
 
-// app.listen(process.env.PORT || 3000, () => {
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
 //   console.log("Client server start");
 // });
